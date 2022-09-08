135115fbf169026d21edb5cdd88e22ba3b99f0e5
Erickype <erickype@hotmail.com>
2022-09-08 10:56:05 -0500

    Function for generate a random id

--- server/main.go
+++ server/main.go
@@ -1,12 +1,20 @@
-package main
+package server
 
 import (
 	"context"
 	pb "github.com/Erickype/GogRPCBasics/proto"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 type server struct {
 	pb.UnimplementedWishlistServiceServer
+}
+
+func generateRandomID() string {
+	rand.Seed(time.Now().Unix())
+	return "ID: " + strconv.Itoa(rand.Int())
 }
 
 func (s *server) createWishlist(ctx context.Context, req *pb.CreateWishlistReq) (*pb.CreateWishlistRes, error) {
