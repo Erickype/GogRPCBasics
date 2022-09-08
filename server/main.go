d19802a36126b873114b226c389f61155aa3a1f1
Erickype <erickype@hotmail.com>
2022-09-08 10:51:36 -0500

    Function declaration for CreateWishlist method

--- server/main.go
+++ server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	pb "github.com/Erickype/GogRPCBasics/proto"
 )
 
 type server struct {
 	pb.UnimplementedWishlistServiceServer
+}
+
+func (s *server) createWishlist(ctx context.Context, req *pb.CreateWishlistReq) (*pb.CreateWishlistRes, error) {
+	return nil, nil
 }
 
 func main() {
