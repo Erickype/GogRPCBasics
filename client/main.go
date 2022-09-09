7a1f77a522eff3795d0a2ee1e7878ce010a824a3
Erickype <erickype@hotmail.com>
2022-09-09 16:32:15 -0500

    Server client implementation

--- client/main.go
+++ client/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	pb "github.com/Erickype/GogRPCBasics/proto"
+	"google.golang.org/grpc"
+)
 
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		panic("Connection error:  " + err.Error())
+	}
+
+	pb.NewWishlistServiceClient(conn)
 }
