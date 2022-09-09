0ffc8845f5668d6fafc080a6fef488f815e3495e
Erickype <erickype@hotmail.com>
2022-09-09 17:10:47 -0500

    Implementation and use of the CreateWishlist method

--- client/main.go
+++ client/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	pb "github.com/Erickype/GogRPCBasics/proto"
 	"google.golang.org/grpc"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -12,5 +17,21 @@ func main() {
 		panic("Connection error:  " + err.Error())
 	}
 
-	pb.NewWishlistServiceClient(conn)
+	clientService := pb.NewWishlistServiceClient(conn)
+
+	res, err := clientService.CreateWishlist(context.Background(), &pb.CreateWishlistReq{
+		Wishlist: &pb.Wishlist{
+			Id:   generateRandomID(),
+			Name: "Erick",
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
+}
+
+func generateRandomID() string {
+	rand.Seed(time.Now().Unix())
+	return "ID: " + strconv.Itoa(rand.Int())
 }
